ddc14dc7d6a30105f0eae1da1a539c84fa6d7c68
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-01-11 11:23:33 -0500

    Display images dynamically from Unsplash

--- script.js
+++ script.js
@@ -8,13 +8,32 @@ const count = 10;
 const apiKey = 'sD6W0vP5mBf0vIvvYsViviYr0Yy0KxnZ6scPg0fm5BQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create elements for links & photos, add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach(photo => {
+    // Create <a> to link to Unsplash 
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+    // Create <img> for photo
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+    // Put <img> inside <a>, then put both inside imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  })
+}
+
 // Get photos from Unspalsh API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
-  } catch (error) {
+    displayPhotos();
+    } catch (error) {
     // Catch error here
   }
 }
